ad1315127db72b35e03a746681d715d8cfccacd8
Marina <mator987@list.ru>
2024-03-26 23:13:42 +0500

    Добавляет верстку слайдера в десктопной версии

--- source/js/modules/juri-swiper.js
+++ source/js/modules/juri-swiper.js
@@ -1,43 +1,40 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
 
-/* import {Navigation, Pagination} from "swiper/modules"; */
-// import 'swiper/css';
 
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
+
   direction: 'horizontal',
   loop: true,
 
   // Navigation arrows
-  /*   navigation: {
+
+  navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  }, */
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
   },
 
   breakpoints: {
 
     320: {
       slidesPerView: 1,
-      spaceBetween: 20
+      spaceBetween: 20,
+      initialSlide: 3,
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
     },
 
     768: {
       slidesPerView: 2,
       spaceBetween: 40,
-      initialSlide: 4,
-
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+      initialSlide: 3,
+      scrollbar: {
+        el: '.swiper-scrollbar',
       },
     },
 
     1366: {
+      initialSlide: 3,
       slidesPerView: 4,
       spaceBetween: 40
     }
